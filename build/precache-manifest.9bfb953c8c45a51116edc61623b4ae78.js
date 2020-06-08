0c54b6a675319e80431b60afd109afcc2d4e5bdc
Patti Shin <pshin518@gmail.com>
2020-06-08 17:11:43 -0400

    Initial commit (create-react-app)

---
+++ build/precache-manifest.9bfb953c8c45a51116edc61623b4ae78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d8ee429771835e85903e5c5b829f579",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa666b6512b05323a11",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4fdb737cc63519bbc174",
+    "url": "/static/js/2.7422a177.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7422a177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa666b6512b05323a11",
+    "url": "/static/js/main.1a5c866c.chunk.js"
+  },
+  {
+    "revision": "37a3c57e631400edd2df",
+    "url": "/static/js/runtime-main.c1b7167b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
