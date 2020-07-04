3b96782b4f2d323b63bbc569ddfc12c08dc7e707
Patti Shin <pshin518@gmail.com>
2020-07-04 17:41:53 -0400

    Clean up

--- src/actions/author.js
+++ src/actions/author.js
@@ -8,10 +8,6 @@ export const getAuthor = () => dispatch => {
 
     return fetch(baseUrl, { method: 'GET' })
       .then(res => res.json())
-      .then(authors => {
-        dispatch({ type: 'GET_AUTHOR_SUCCESS', payload: authors });
-      })
-      .catch(err => {
-        dispatch({ type: 'GET_AUTHOR_FAILURE', payload: err });
-      });
+      .then(authors => dispatch({ type: 'GET_AUTHOR_SUCCESS', payload: authors }))
+      .catch(err => dispatch({ type: 'GET_AUTHOR_FAILURE', payload: err }));
 }
